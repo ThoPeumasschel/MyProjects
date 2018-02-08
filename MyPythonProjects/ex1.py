60e35ebeb7d97dd6a779bc1b915496fbab6922c1
ThoPeumasschel <tpeuschel@web.de>
2018-02-08 12:57:14 +0100

    restored?

--- MyPythonProjects/ex1.py
+++ MyPythonProjects/ex1.py
@@ -6,16 +6,4 @@ print 'Yay! Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
 print 'Now let\'s change something.'
-<<<<<<< HEAD
-=======
-print "Hello World!"
-print "Hello Again"
-print "I like typing this"
-print "This is fun"
-print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
->>>>>>> 3ed5b224391a07a2fccae75ff065875694debe41
-=======
 print "I typed this changes directly on github!"
->>>>>>> 37db621bceb7faeb7317f5c53b85ff2f8362bd6d
