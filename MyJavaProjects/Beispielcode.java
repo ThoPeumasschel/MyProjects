4eb67d1e881f446d7f9d6e8fc80816f5e6f18187
ThoPeumasschel <tpeuschel@web.de>
2018-02-09 03:32:08 +0100

    ... instead of []

--- MyJavaProjects/Beispielcode.java
+++ MyJavaProjects/Beispielcode.java
@@ -1,22 +1,14 @@
-/* Hier gibt's mal ein bischen Beispielcode. 
+/* Hier gibt's mal ein bischen Beispielcode.
  * Einfach eine Klassendeklaration mit main-function.
- */ 
-
+ */
 
 class Beispielcode {
-
-	public static void main( String[] args ) {
-
+	public static void main( String... args ) {
 		String text = "\nHallo Welt";
-
 		System.out.println( text );
-
 		System.out.print( "1.2 + 1.0 = " );
-
 		System.out.println( 1.2 + 1.0 );
-
-		System.out.println("Jetzt wollen wir doch mal sehen, ob ich diesen Text gepuscht kriege");
-
+		System.out.println("Jetzt wollen wir doch mal sehen,
+		ob ich diesen Text gepuscht kriege");
 	}
-
 }
