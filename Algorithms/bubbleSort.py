68594270f5ea33965f009e3132a7d7615f4fa246
ThoPeumasschel <tpeuschel@web.de>
2018-02-13 22:25:37 +0100

    finished bubble sort

---
+++ Algorithms/bubbleSort.py
@@ -0,0 +1,12 @@
+# bubble sort for python
+
+def bubble_sort(nlist):
+	for passnum in range(len(nlist)-1,0,-1):
+		for j in range(passnum):
+			if nlist[j]>nlist[j+1]:
+				nlist[j], nlist[j+1] = nlist[j+1], nlist[j]
+
+nlist = [14, 46, 27, 57, 41, 45, 21, 70]
+bubble_sort(nlist)
+print(nlist)
+
