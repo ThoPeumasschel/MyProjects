8999469c3e88bc7d79277f7981e96828e408fcdb
ThoPeumasschel <tpeuschel@web.de>
2018-02-20 10:03:57 +0100

    Heizung befÃllt

---
+++ MyJavaProjects/Letter.java
@@ -0,0 +1,37 @@
+//import java.util.*
+//import java.text.*
+
+public class Letter {
+	public static void main(String[] args) {
+//		SimpleDateFormat date=new SimpleDateFormat("HH:mm:ss");
+//		String Uhrzeit=date.format(new Date());
+
+		String name = "Lieschen Mueller";
+		char sex = 'w';
+		int currentHour = 10;
+		String m = "Guten Morgen";
+		String t = "Guten Tag";
+		String a = "Guten Abend";
+
+
+
+
+		if (sex == 'm') {
+			System.out.println(m + " " + name + 
+
+		
+		else if (currentHour <= 9) {
+			switch m/w
+				"Guten Morgen"
+			}
+		else if (currentHour > 9) {
+			"Guten Tag"
+			}
+		else if (currentHour > 17) {
+			"Guten Abend"
+			}
+
+
+
+
+
---
+++ MyJavaProjects/Maximum.java
@@ -0,0 +1,11 @@
+public class Maximum {
+	public static void main(String... args) {
+		int a = 543; int b = 246;
+		if (a > b) {
+			System.out.println(a + " ist groesser als " + b);
+		}
+		else {
+			System.out.println(b + " ist groesser als " + a);
+		}
+	}
+}
