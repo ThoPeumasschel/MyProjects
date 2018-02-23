4b7655a10339e7b43ebb9738ab71812cbece4fae
ThoPeumasschel <tpeuschel@web.de>
2018-02-23 14:18:25 +0100

    chess

---
+++ MyJavaProjects/KingChess.java
@@ -0,0 +1,22 @@
+
+public class KingChess {
+
+	static double countWheat (double x) {
+		double x = 1;
+		if (a < 64) 
+			return (countWheat(x * 2));
+		else
+			return 0;
+
+
+	}	
+
+public static void main(String...args) {
+	
+	double koerner = 1;
+	double felder = 64;
+	countWheat(n);
+
+
+
+
