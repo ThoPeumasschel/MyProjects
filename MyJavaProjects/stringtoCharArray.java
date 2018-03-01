c7f98f2b11c174f1637c22e8c0f0259cceb2ff44
ThoPeumasschel <tpeuschel@web.de>
2018-03-01 15:52:04 +0100

    nachtrag...

---
+++ MyJavaProjects/stringtoCharArray.java
@@ -0,0 +1,13 @@
+
+class versuch {
+	
+	public static void main(String...args) {
+
+		String string = "Herr, schmeiß Java vom Himmel!";
+
+			for ( char c : string.toCharArray() )
+
+				System.out.println( c );
+
+}
+}
