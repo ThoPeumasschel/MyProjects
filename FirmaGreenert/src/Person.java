74f4ebf90264c40737f2c62fa34f109bf91000bd
ThoPeumasschel <tpeuschel@web.de>
2018-03-14 13:00:32 +0100

    14.03.2018

--- FirmaGreenert/src/Person.java
+++ FirmaGreenert/src/Person.java
@@ -1,8 +1,7 @@
 import java.io.*;
+import java.util.ArrayList;
 
-
-
-public class Person {
+public class Person extends Firma implements Serializable {
 	private String name, vorname, adresse, telefonnr;
 	public final int ANZPERSONEN = 30;
 	public int iang = 0, iarb = 0;
@@ -14,6 +13,7 @@ public class Person {
 		vorname = null;
 		adresse = null;
 		telefonnr = null;
+		einlesen();
 	}
 
 	Person(String n, String v, String a, String t) {
@@ -21,6 +21,7 @@ public class Person {
 		vorname = v;
 		adresse = a;
 		telefonnr = t;
+		einlesen();
 	}
 
 	String getName() {
@@ -59,6 +60,29 @@ public class Person {
 		return "Name:\t\t" + name + ", " + vorname + "\nAdresse:\t" + adresse + "\nTelefonnr:\t" + telefonnr + "\n";
 	}
 
+	void einlesen() {
+		try {
+
+			FileInputStream fs = new FileInputStream("Angestellte.ser");
+			ObjectInputStream os = new ObjectInputStream(fs);
+			ang = (Angestellter[]) os.readObject();
+			for (int i = 0; ang[i] != null; ++i)
+				iang = i + 1;
+			os.close();
+			FileInputStream fs1 = new FileInputStream("Arbeiter.ser");
+			ObjectInputStream os1 = new ObjectInputStream(fs1);
+			arb = (Arbeiter[]) os1.readObject();
+			for (int i = 0; arb[i] != null; ++i)
+				iarb = i + 1;
+			os1.close();
+			anzMitarb = iang + iarb;
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.toString());
+		}
+	}
+	
 	void menue() {
 		int cEingabe;
 
@@ -101,11 +125,31 @@ public class Person {
 			}
 			case 5: {
 				System.out.println("Aktuell sind " + iang + " Angestellte und " + iarb + " Arbeiter beschäftigt.");
-				System.out.println("Also insgesamt " + (iang+iarb) + " Mitarbeiter.");
+				System.out.println("Also insgesamt " + (iang + iarb) + " Mitarbeiter.");
+				System.out.println("Und nochmal mit anzMitarb-Variable " + anzMitarb);
+				break;
 			}
 
 			}
 		} while (cEingabe != 0);
+		
+		try {
+			FileOutputStream fs = new FileOutputStream("Angestellte.ser");
+			ObjectOutputStream os = new ObjectOutputStream(fs);
+			os.writeObject(ang);
+			os.close();
+			
+			FileOutputStream fs1 = new FileOutputStream("Arbeiter.ser");
+			ObjectOutputStream os1 = new ObjectOutputStream(fs1);
+			os1.writeObject(arb);
+			os1.close();
+
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		}
+
+		
+		
 	}
 
 	void getAngestellte() {
@@ -115,11 +159,14 @@ public class Person {
 		ang1.setAdresse(readString("Adresse"));
 		ang1.setTelefonnr(readString("Telefon"));
 		ang1.setMonatsgehalt(Integer.parseInt(readString("Monatsgehalt")));
-		
+
 		if (iang < ANZPERSONEN) {
 			ang[iang++] = ang1;
-		} else 
+			anzMitarb++;
+		} else
 			System.err.println("Array für Angestellte ist voll!");
+		if (anzMitarb >= MAXMITARB)
+			System.err.println("Maximale Mitarbeiterzahl ist erreicht");
 
 	}
 
@@ -127,28 +174,33 @@ public class Person {
 		Arbeiter arb1 = new Arbeiter();
 		if (iarb < ANZPERSONEN) {
 			arb[iarb++] = arb1;
-		} else 
+			anzMitarb++;
+		} else
 			System.err.println("Array für Angestellte ist voll!");
-		
+		if (anzMitarb >= MAXMITARB)
+			;
+		System.out.println("Maximale Mitarbeiterzahl ist erreicht");
+
 		arb1.setName(readString("Name"));
 		arb1.setVorname(readString("Vorname"));
 		arb1.setAdresse(readString("Adresse"));
 		arb1.setTelefonnr(readString("Telefon"));
 		arb1.setStundenlohn(Integer.parseInt(readString("Stundenlohn")));
 		arb1.setAnzahlstd(Integer.parseInt(readString("Anzahl Stunden")));
-		
 
 	}
 
 	void angAuflisten() {
-		for (int i = 0; i < iang; i++) {
+		
+		for (int i = 0; ang[i] != null; ++i) {
 			System.out.println(ang[i].zeigePerson());
+		
 		}
 
 	}
 
 	void arbAuflisten() {
-		for (int i = 0; i < iarb; i++) {
+		for (int i = 0;arb[i] != null; ++i) {
 			System.out.println(arb[i].zeigePerson());
 		}
 	}
