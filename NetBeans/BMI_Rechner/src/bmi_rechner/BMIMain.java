5b77363e85a41b23c049b263df1ddb1da28cba36
ThoPeumasschel <tpeuschel@web.de>
2018-05-10 16:47:29 +0200

    Finished BMI_Rechner

---
+++ NetBeans/BMI_Rechner/src/bmi_rechner/BMIMain.java
@@ -0,0 +1,40 @@
+package bmi_rechner;
+
+public class BMIMain {
+// Quelle der Formel und der verwendeten Texte: www.apotheken-umschau.de/bmi-rechner
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Eingabe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Eingabe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Eingabe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Eingabe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            // Die Applikation wird gestartet
+            public void run() {
+                // setzt die Sichtbarkeit des Fensters auf true
+                new Eingabe().setVisible(true);
+            }
+        });
+    }
+}
+
+// Quelle der Formel und der verwendeten Texte: www.apotheken-umschau.de/bmi-rechner
