cdb2818d9ee33a701da58772d2c9859f18a6eb51
ThoPeumasschel <tpeuschel@web.de>
2018-05-13 20:45:32 +0200

    Hausi fertig!

--- NetBeans/BMI_Oop/src/bmi_oop/BMI_Oop.java
+++ NetBeans/BMI_Oop/src/bmi_oop/BMI_Oop.java
@@ -6,7 +6,7 @@
 package bmi_oop;
 
 import java.text.DecimalFormat;
-import javax.swing.JOptionPane;
+
 
 /**
  *
@@ -17,21 +17,28 @@ public class BMI_Oop {
    
     public static void main(String[] args) {
         DecimalFormat df = new DecimalFormat("0");
-        
+        // Es werden Instanzen der Klassen erzeugt
         Eingabe e = new Eingabe();
         Ausgabe a = new Ausgabe();
         Speichern s = new Speichern();
         Berechnung b = new Berechnung();
         Klassifikation k = new Klassifikation();
-        
+        // Von Objekt 'e' wird die Methode 'eingeben()' aufgerufen
         e.eingeben();
-        //System.out.println(e.name + ", " + e.geschlecht + " Gewicht: " + e.gewicht + "Größe: " + e.groesse);
+        // Der BMI wird durch den Aufruf der Methode 'berechne()' der Instanz b 
+        // aus den in 'e' gespeicherten Werten errechnet und in 'BMI' gespeichert
         double BMI = (b.berechne(e.gewicht, e.groesse));
-        
-        //System.out.println("BMI: " + df.format(BMI));
+        // Von Objekt 'k' wird die Methode 'klassifizieren()' mit den Parametern 
+        // 'e' und 'BMI' aufgerufen. Aus den Angaben Geschlecht und BMI wird nach der
+        //Tabelle klassifiziert und das Ergebnis hier in 'klass' gespeichert. 
         String klass = k.klassifizieren(e, BMI);
-        //System.out.println("Klassifizierung: " + klass);
+        // Die Benutzereingaben werden zusammen mit BMI und Klassifikation durch 
+        // den Aufruf der Methode 'ausgeben()' von 'a' mit den Argumenten 'e' 
+        // und 'klass' ausgegeben. 
         a.ausgeben(e, klass);
+        // Die Eingaben werden mit den Ergebnissen und der aktuellen Zeit in einer 
+        // Datei gespeichert. Man beachte, dass einigen Funktionen Instanzen von 
+        // anderen Klassen als Argumente übergeben werden. 
         s.speichern(e, BMI, klass);
         
     }
--- NetBeans/BMI_Oop/src/bmi_oop/Speichern.java
+++ NetBeans/BMI_Oop/src/bmi_oop/Speichern.java
@@ -1,31 +1,33 @@
 package bmi_oop;
 
 import java.awt.HeadlessException;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.text.DecimalFormat;
-import javax.swing.JOptionPane;
+import java.time.*;
+import java.time.format.*;
 
 public class Speichern {
 
     void speichern(Eingabe e, double BMI, String k) {
         DecimalFormat df = new DecimalFormat("0");
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dateform;
+        dateform = DateTimeFormatter.ofPattern("dd.MM.yyyy kk:mm");
+       
         try {
             // Datei wird erstellt oder vorhandene Datei zum schreiben geöffnet. 
             File f = new File("BMI-Daten.txt");
-            //File g = new File("BMI-Daten.txt");
-            // Damit die Daten zugeordnet werden können, wird hier noch der Name abgefragt. 
-
             // In einem neuen try-catch-block wird eine Instanz der FileWriter-Klasse
             // gebildet, mit der sichergestellt wird, dass die neuen Daten angehängt werden. 
             try (FileWriter w = new FileWriter(f, true)) {
                 w.write("BMI Ergebnis für " + e.name + "\r\nGeschlecht: " + e.geschlecht
                     + "\r\nGröße: " + e.groesse + "cm\r\nGewicht: " + e.gewicht
-                    + "Kg\r\nBMI = " + df.format(BMI) + " bedeutet " + k + "\r\n\r\n");
+                    + "Kg\r\nBMI = " + df.format(BMI) + " bedeutet " + k + "\r\n" 
+                    + "Zeit + Datum: " + now.format(dateform) + "\r\n     ---------------     \r\n");
             }
 
-        } catch (@SuppressWarnings("LocalVariableHidesMemberVariable") HeadlessException | IOException I) {
+        } catch (@SuppressWarnings("LocalVariableHidesMemberVariable") 
+                HeadlessException | IOException I) {
         }
 
     }
