02bdee6938be46948d1fd56519bbff8d54eb5d4a
Thomas Peuschel <tpeuschel@web.de>
2018-05-23 12:58:52 +0200

    23.05.2018

---
+++ NetBeans/ArrayListKunde/src/arraylistkunde/Arrays3.java
@@ -0,0 +1,78 @@
+package arraylistkunde;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Arrays3 {
+
+    static ArrayList<Kunde> kundenListe = new ArrayList<Kunde>();
+
+    public static void main(String[] args) {
+        byte menu = 0;
+        do {
+            System.out.println("1 - Kunde anlegen");
+            System.out.println("2 - Kunden ausgeben");
+            System.out.println("3 - Anwendung beenden");
+
+            menu = new Scanner(System.in).nextByte();
+
+            switch (menu) {
+                case 1:
+                    kundenAnlegen();
+                    break;
+
+                case 2:
+                    kundenAusgeben();
+                    break;
+
+                case 3:
+                    System.out.println("Anwendung wird beendet");
+                    break;
+                default:
+                    break;
+            }
+
+        } while (menu != 3);
+    }
+
+    private static void kundenAnlegen() {
+        //nr, nachname, vorname
+        System.out.println("Kundennummer: ");
+        String nr = new Scanner(System.in).next();
+        System.out.println("Nachname: ");
+        String nachname = new Scanner(System.in).next();
+        System.out.println("Vorname: ");
+        String vorname = new Scanner(System.in).next();
+
+        Kunde kunde = new Kunde();
+        kunde.setNr(nr);
+        kunde.setNachname(nachname);
+        kunde.setVorname(vorname);
+        
+        kundenListe.add(kunde);
+        /*
+        kundenListe.add(new String[3]);
+        kundenListe.get(0)[0] = "Test";*/
+    }
+
+    private static void kundenAusgeben() {
+
+        for (Kunde s : kundenListe) {
+            System.out.println(s.getNr() + " - " + s.getNachname() + " - " + s.getVorname());
+        }
+
+        /*
+        for (int i = 0; i < z; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(kundenArray[i][j]);
+            }
+            System.out.println();
+
+        }
+         */ //   String[] strings = kundenArray[i];
+        {
+
+        }
+    }
+
+}
---
+++ NetBeans/ArrayListKunde/src/arraylistkunde/Kunde.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraylistkunde;
+
+
+ 
+public class Kunde {
+    
+    private String nr, nachname, vorname;
+
+    public String getNr() {
+        return nr;
+    }
+
+    public void setNr(String nr) {
+        this.nr = nr;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+    
+    
+}
