0af56574df135521270ccc5b21fbb4a6e12283ef
Thomas Peuschel <tpeuschel@web.de>
2018-07-02 15:08:00 +0200

    Generics

--- Hasanovic/AufgabeZugplanPeu/src/zugplan/LinienSortiererNachZeit.java
+++ Hasanovic/AufgabeZugplanPeu/src/zugplan/LinienSortiererNachZeit.java
@@ -5,6 +5,11 @@ import java.util.Comparator;
 public class LinienSortiererNachZeit implements Comparator
 {
 
+	public void orderByTime(Zug z)
+	{
+
+	}
+
 	@Override
 	public int compare(Object o1, Object o2)
 	{
