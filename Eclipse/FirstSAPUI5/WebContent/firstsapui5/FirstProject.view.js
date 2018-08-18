a93e71ae2a50cbce0b90dfdd1320308514f75aea
ThoPeumasschel <tpeuschel@web.de>
2018-08-18 07:59:40 +0200

    Hello from the basement...

---
+++ Eclipse/FirstSAPUI5/WebContent/firstsapui5/FirstProject.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("firstsapui5.FirstProject", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf firstsapui5.FirstProject
+	*/ 
+	getControllerName : function() {
+		return "firstsapui5.FirstProject";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf firstsapui5.FirstProject
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
