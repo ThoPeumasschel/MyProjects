27463df5987a8aebd0baa8b549cb1f8c9090b3b0
ThoPeumasschel <tpeuschel@web.de>
2018-11-06 20:10:51 +0100

    ökjfbgökajdgökjadsnögkj

---
+++ MyCProjects/ErrorCodes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <errno.h>
+
+#include <string.h>
+
+
+
+extern int errno;
+
+
+
+int main() {
+
+    for (int x = 0; x < 135; x++) {
+      fprintf(stderr, "%d: %s\n", x, strerror(x));
+    }
+
+
+}
---
+++ MyCProjects/SortingWithFunctionPointers.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+int compare (const void *, const void *); 
+
+
+
+int main() {
+    
+    
+	int arr1[5] = {52, 23, 56, 19, 4};
+    
+    
+	int *arr2[5];
+    
+	int k = 0;
+    
+	printf("Enter five numbers:");
+    
+	for(int j = 0; j < 5; j++) {
+        
+		printf("%d) ", j + 1);
+        
+		scanf("%d", &k);
+        
+		arr2[j] = k;
+    
+	}
+   
+
+    
+    
+    
+    
+	int num, width, i;
+    
+	printf("before sorting: \n");
+     
+	for (i = 0; i < 5; i++) {
+        
+		printf("%d ", arr2[ i ]);
+    
+	}
+    
+	printf("\nafter sorting:\n");
+    
+    
+	num = sizeof(arr2)/sizeof(arr2[0]);
+    
+	width = sizeof(arr2[0]);
+    
+	qsort((void *)arr2, num, width, compare);
+    
+	for (i = 0; i < 5; i++) {
+        
+		printf("%d ", arr2[ i ]);
+    
+	}
+    
+	return 0;
+
+}
+
+
+
+int compare (const void *elem1, const void *elem2) {
+    
+	if ((*(int *)elem1) == (*(int *)elem2))
+        
+		return 0;
+    
+	else if ((*(int *)elem1) < (*(int *)elem2))
+        
+		return -1;
+    
+	else
+        
+		return 1;
+} 
